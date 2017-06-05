bfabf7c0ee6849e6ee0d2e495a0967433552f243
summerbreez <leannecheung00@gmail.com>
2017-06-05 08:45:28 -0600

    able to convert to on message to promise

--- app.js
+++ app.js
@@ -8,20 +8,15 @@ const Secrets = require('./secrets');
 const secrets = new Secrets();
 const redFront = ssredis.createClient(secrets.mongred.redis1Port, secrets.mongred.ip)
 
-// const bluebird = require('bluebird')
-
 // create a bot
 var bot = new SlackBot({
     token: 'xoxb-192390456148-MSAb5sKsjsDhlEfQMet1svhN', // Add a bot https://my.slack.com/services/new/bot and put the token
     name: 'testbot2'
 });
 
-const test123 = function(){
+const onStart = function(){
     return new Promise(function(resolve, reject){
-        bot.on('start', function(err, response, body){
-            if(err)
-                return reject(err)
-
+        bot.on('start', function(data){
 
             var params = {
                 icon_emoji: ':cat:'
@@ -35,7 +30,40 @@ const test123 = function(){
     })
 }
 
-test123()
+const onMessage = function(){
+    return new Promise(function(resolve, reject){
+        bot.on('message', function(data){
+            switch (data.text){
+                case 'help':
+                    bot.postMessageToChannel('general', 'here are the info')
+                    break
+                case 'testbot2 listItems':
+                    bot.postMessageToChannel('general', 'lists')
+                    break
+                case 'testbot2 turnOnItem':
+                    bot.postMessageToChannel('general', 'turned on item')
+                    break
+                case 'testbot2 turnOffItem':
+                    bot.postMessageToChannel('general', 'turned off item')
+                    break
+                default:
+                    break
+            }
+
+            resolve(data.text)
+
+        })
+    })
+}
+
+// onStart()
+onMessage().then(function(data1){
+    console.log('data1', data1)
+})
+
+// async function startMessage(){
+//     await onMessage()
+// }
 
 // bot.on('message', function(data){
 //     console.log(data.text)
